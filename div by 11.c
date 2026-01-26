2b0ebabdd00666e95b82efbe70beef035e3a5799
priyadharshinirangadurai56-lab <priyadharshinirangadurai56@gmail.com>
2026-01-26 22:33:36 +0530

    Add files via upload
    div by 11.c

---
+++ div by 11.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (num % 11 == 0) {
+        printf("Divisible by 11");
+    }
+    else {
+        printf("Not divisible by 11");
+    }
+    return 0;
+}
