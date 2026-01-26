ebb2385ce8a94f84fd02332395d5f8a7253f4dc3
priyadharshinirangadurai56-lab <priyadharshinirangadurai56@gmail.com>
2026-01-26 22:38:45 +0530

    Add files via upload
    switch.c

---
+++ switch.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+    switch(ch) {
+        case 'a': case 'e': case 'i': case 'o': case 'u':
+        case 'A': case 'E': case 'I': case 'O': case 'U':
+            printf("Vowel\n");
+            break;
+        default:
+            printf("Not a vowel\n");
+    }
+    return 0;
+}
