e2c64f6de86064c3c86077e6b706faeeaf4c6536
priyadharshinirangadurai56-lab <priyadharshinirangadurai56@gmail.com>
2026-01-28 08:10:46 +0530

    Add files via upload
    buspass switch.c

---
+++ buspass switch.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+    int category, distance;
+    scanf("%d", &category);
+    scanf("%d", &distance);
+    switch (category) {
+        case 1:   
+            if (distance <= 30)
+                printf("Eligible");
+            else
+                printf("Eligible with Extra Fee");
+            break;
+        case 2:   
+            if (distance <= 30)
+                printf("Eligible");
+            else
+                printf("Not Eligible");
+            break;
+        default:  
+            if (distance <= 30)
+                printf("Eligible");
+            else
+                printf("Not Eligible");
+    }
+    return 0;
+}
