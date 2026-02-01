9e7bba96edc255bfa9b36207d1cf731a4b0279e5
priyadharshinirangadurai56-lab <priyadharshinirangadurai56@gmail.com>
2026-02-01 20:23:27 +0530

    Add files via upload
    mobile fl.c

---
+++ mobile fl.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int batteryPercent, N;
+    int drain, i;
+    scanf("%d", &batteryPercent);
+    scanf("%d", &N);
+    for (i = 0; i < N; i++) {
+        scanf("%d", &drain);
+        batteryPercent -= drain;
+    }
+    printf("Remaining Battery: %d%%", batteryPercent);
+    return 0;
+}
