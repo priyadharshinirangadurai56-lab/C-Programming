a5ce0f4b0a3cc9a0cce764c3eca3a0572dc5c93d
priyadharshinirangadurai56-lab <priyadharshinirangadurai56@gmail.com>
2026-02-03 21:10:31 +0530

    Add files via upload
    spacecraft while.c

---
+++ While_loop/spacecraft while.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+    int fuel, N;
+    scanf("%d", &fuel);
+    scanf("%d", &N);
+    int i = 1, burn;
+    int emergencyStage = -1;
+    int fuelWasted = 0;
+    while (i <= N) {
+        scanf("%d", &burn);
+        fuel = fuel - burn;
+        if (fuel < 0 && emergencyStage == -1) {
+            emergencyStage = i;
+            fuelWasted = -fuel;
+        }
+        i++;
+    }
+    if (emergencyStage == -1)
+        printf("Emergency Stage: Not Occurred\n");
+    else
+        printf("Emergency Stage: %d\n", emergencyStage);
+
+    printf("Fuel Wasted: %d\n", fuelWasted);
+    return 0;
+}
