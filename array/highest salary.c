4cda37f35a5d2dde4b8a10c8e0bb47d4103f7f5d
priyadharshinirangadurai56-lab <priyadharshinirangadurai56@gmail.com>
2026-02-04 21:58:39 +0530

    Add files via upload
    highest salary.c

--- array/highest salary.c
+++ array/highest salary.c
@@ -1,1 +1,25 @@
-
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int max = -1, second = -1;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] > max) {
+            second = max;
+            max = arr[i];
+        }
+        else if(arr[i] < max && arr[i] > second) {
+            second = arr[i];
+        }
+    }
+    if(second == -1) {
+        printf("Second highest salary not found");
+    } else {
+        printf("%d", second);
+    }
+    return 0;
+}
