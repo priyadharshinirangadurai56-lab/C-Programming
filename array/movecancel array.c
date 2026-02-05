f97aae618d872b677ffd54ce5fc75cae464ef67c
priyadharshinirangadurai56-lab <priyadharshinirangadurai56@gmail.com>
2026-02-05 22:00:09 +0530

    Add files via upload
    movecancel array.c

---
+++ array/movecancel array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int k = 0;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] != 0) {
+            arr[k] = arr[i];
+            k++;
+        }
+    }
+    while(k < n) {
+        arr[k] = 0;
+        k++;
+    }
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
