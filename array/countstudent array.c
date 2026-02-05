bef711dd58bbeefa4a4a60d832e804cb803fdce2
priyadharshinirangadurai56-lab <priyadharshinirangadurai56@gmail.com>
2026-02-05 21:58:55 +0530

    Add files via upload
    countstudent array.c

---
+++ array/countstudent array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    int sum = 0;
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+    int average = sum / n;
+    int count = 0;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] > average) {
+            count++;
+        }
+    }
+    printf("%d", count);
+    return 0;
+}
