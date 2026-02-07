125366b5124e9db4c440ea3d6ae2a21767cb79c9
priyadharshinirangadurai56-lab <priyadharshinirangadurai56@gmail.com>
2026-02-07 22:11:00 +0530

    Add files via upload
    replace array.c

---
+++ array/replace array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int N;
+    scanf("%d", &N);
+    int arr[N];
+    for(int i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for(int i = 0; i < N; i++) {
+        if(arr[i] < 0) {
+            arr[i] = 0;
+        }
+    }
+    for(int i = 0; i < N; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
