7ecef64a89b8504f83de2c340f72d57d02daf44c
priyadharshinirangadurai56-lab <priyadharshinirangadurai56@gmail.com>
2026-02-07 22:11:44 +0530

    Add files via upload
    first repeat array.c

---
+++ array/first repeat array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int N;
+    scanf("%d", &N);
+    int arr[N];
+    for(int i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for(int i = 0; i < N; i++) {
+        for(int j = i + 1; j < N; j++) {
+            if(arr[i] == arr[j]) {
+                printf("%d", arr[i]);
+                return 0;
+            }
+        }
+    }
+    printf("-1");
+    return 0;
+}
