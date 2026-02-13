c0bd9e21e560b9b04b67f53234d9694a32a57045
priyadharshinirangadurai56-lab <priyadharshinirangadurai56@gmail.com>
2026-02-13 21:31:22 +0530

    Add files via upload
    lowtoupper.c

---
+++ string/lowtoupper.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    char str[1000];
+    int i = 0;
+    printf("Enter the string: ");
+    fgets(str, sizeof(str), stdin);
+    while (str[i] != '\0') {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - ('a' - 'A');
+        }
+        i++;
+    }
+    printf("Uppercase string: %s", str);
+    return 0;
+}
