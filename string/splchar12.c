e156d047851520cc563d0b775978aee5047eedb6
priyadharshinirangadurai56-lab <priyadharshinirangadurai56@gmail.com>
2026-02-13 21:33:39 +0530

    Add files via upload
    splchar12.c

---
+++ string/splchar12.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    int i=0;
+    char str[100];
+    int count = 0;
+    scanf("%s", str);
+    while(str[i] != '\0') {
+        if(str[i] == '@' || str[i] == '&' || str[i] == '%' || str[i] == '$' ||str[i] == '#') {
+            count++;
+        }
+        i++;
+    }
+    printf("%d", count);
+
+    return 0;
+}
