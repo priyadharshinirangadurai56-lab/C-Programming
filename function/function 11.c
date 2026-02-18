e088ffe9115f7441c3e76c485ad5dcddd3427789
priyadharshinirangadurai56-lab <priyadharshinirangadurai56@gmail.com>
2026-02-18 21:49:55 +0530

    Add files via upload
    function 11.c

---
+++ function/function 11.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int countDigits(int n) {
+    int count = 0;
+    while (n != 0) {
+        n = n / 10;  
+        count++;
+    }
+    return count;
+}
+int main() {
+    int n;
+    scanf("%d", &n);
+    if (n == 0) {
+        printf("1\n");
+    } else {
+        int result = countDigits(n);
+        printf("%d\n", result);
+    }
+    return 0;
+}
